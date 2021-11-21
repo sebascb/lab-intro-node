dbecb4d2454aefee292345e26160bee3ea9cda22
Sebas Corral <sebascorralbrito@gmail.com>
2021-11-21 22:55:01 +0100

    done

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let newArr = this.items;
+    newArr.push(item)
+    newArr.sort((a, b) => {
+      return a-b;
+    })
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error ("OutOfBounds")
+    }
+      return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+     return this.items.sort((a, b) => (a - b))[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+     return this.items.sort((a, b) => (b - a))[this.length - 1];
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+      return this.items.reduce((a,b) => a + b, 0)
+  }
 
   avg() {}
 }
